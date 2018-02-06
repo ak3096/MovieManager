4843dafc0e39a23712386357024d1b201fab1e34
Akshay <akshay.talwar123@gmail.com>
2018-02-06 20:28:53 +0530

    Initial commit

---
+++ app/src/main/java/com/androidlove/moviemanager/activities/MovieDetailActivity.java
@@ -0,0 +1,58 @@
+package com.androidlove.moviemanager.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidlove.moviemanager.R;
+import com.androidlove.moviemanager.models.Movie;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+@SuppressWarnings("ALL")
+public class MovieDetailActivity extends AppCompatActivity {
+
+    Movie movie;
+    @BindView(R.id.ivMovieBackdrop)
+    ImageView ivMovieBackdrop;
+    @BindView(R.id.tvOverview)
+    TextView tvOverview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+        ButterKnife.bind(this);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Movie saved as favorite", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            movie = (Movie) extras.getSerializable("Movie");
+            this.setTitle(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+
+            Picasso.with(this)
+                    .load(movie.getBackdropPath())
+                    .into(ivMovieBackdrop);
+        }
+    }
+
+}
